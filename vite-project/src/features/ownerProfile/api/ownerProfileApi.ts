f6f5edb9980ee9074a6eec33541f9246f456c20d
Yosef Steinberg <steinbergyosef@gmail.com>
2026-07-23 13:59:24 +0300

    working on profilePageFeature

---
+++ vite-project/src/features/ownerProfile/api/ownerProfileApi.ts
@@ -0,0 +1,43 @@
+import api from "../../../lib/axiosClient";
+
+export const ownerProfileApi = {
+
+  checkBookings: function(userId:string){
+    return api.get(`/api/bookings/checkIfUserHasBookings/${userId}`);
+  },
+
+  fetchDriveways: function(userId:string){
+    return api.get(`/api/driveways/getAllDrivewaysByUserId/${userId}`);
+  },
+
+  checkStripeVerification: function(userId:string){
+    return api.get(`/api/users/${userId}/stripe/check-verification`);
+  },
+
+  fetchGames: function(drivewayId:string){
+    return api.get(`/api/driveways/getGames/${drivewayId}`);
+  },
+
+  updateFirstName: function(userId:string, firstName:string){
+    return api.put(`/api/users/${userId}/firstName/${firstName}`);
+  },
+
+  updateLastName: function(userId:string, lastName:string){
+    return api.put(`/api/users/${userId}/lastName/${lastName}`);
+  },
+
+  updateEmail: function(userId:string, email:string){
+    return api.put(`/api/users/${userId}/email/${email}`);
+  },
+
+  blockGame: function(drivewayId: string, gameDate:string){
+    return api.put(`/api/driveways/${drivewayId}/block/${gameDate}`);
+  },
+
+  unblockGame: function(drivewayId: string, gameDate:string){
+    return api.put(`/api/driveways/${drivewayId}/unblock/${gameDate}`);
+  },
+
+  
+
+};
